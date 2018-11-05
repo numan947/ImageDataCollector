53d9b3e469a89ff3578dacbef4a20e44d2d404be
numan947 <mahmudulhasan947@gmail.com>
2018-11-05 15:02:41 +0600

    Custom Icons working, half completed ui for home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Camera, CameraOptions} from "@ionic-native/camera";
+
+
+
+
+
 
 @Component({
   selector: 'page-home',
@@ -13,10 +19,38 @@ export class HomePage {
   /**
    * @ignore
    * */
-  rafidProject:boolean = true;
+  rafidProject:boolean = false;
 
-  constructor(public navCtrl: NavController) {
 
+ CAMERAOPTIONS:CameraOptions = {
+    quality:100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.PNG,
+    mediaType: this.camera.MediaType.PICTURE,
+    sourceType: this.camera.PictureSourceType.CAMERA
+  };
+
+  CROPPEROPTIONS:any ={
+    dragMode: 'crop',
+    aspectRatio: 1,
+    autoCrop: true,
+    movable:true,
+    zoomable: true,
+    scalable: true,
+    autoCropArea:0.8
+  };
+
+  capturedImage:any = null;
+  croppedImage:any = null;
+
+  constructor(public navCtrl: NavController, private camera:Camera) {
+
+  }
+
+  captureImage(){
+    this.camera.getPicture(this.CAMERAOPTIONS).then(imageData=>{
+      this.capturedImage = 'data:image/png;base64'+imageData;
+    });
   }
 
 
